c091dda66ad79e81e962bc0cb4b43afaf7523df0
Alamin Musa Magaga <55305080+alaminmagaga@users.noreply.github.com>
2022-06-20 17:57:18 -0700

    Update settings.py

--- pocketdoc/settings.py
+++ pocketdoc/settings.py
@@ -71,7 +71,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [('172.20.0.1', 6379)],
         },
     },
 }             
